e6f0a5b8a6ed636022b46a5e965256955b213bb0
Pascal Nguyen <pasngu@student.42.fr>
2019-02-17 17:34:37 +0100

    Double array burger

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,11 +4,24 @@ import classes from './Burger.css'
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 
 const burger = (props) => {
+    
+    // const transformedIngredient = Object.keys(props.ingredients)
+    const transformedIngredients = Object.keys(props.ingredients)
+        .map(igkey => {
+            return [...Array(props.ingredients[igkey])].map((_, i) => {
+                return <BurgerIngredient key={igkey + i} type={igkey} />
+            })
+        })
+
+    // console.log(transformedIngredient)
+    // console.log(transformedIngredients)
+    // console.log(props.ingredients)
+    // console.log(props.ingredients['salad'])
+    // console.log(props.ingredients.salad)
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type='bread-top' />
-            <BurgerIngredient type='cheese' />
-            <BurgerIngredient type='meat' />
+            {transformedIngredients}
             <BurgerIngredient type='bread-bottom' />
         </div>
     )
