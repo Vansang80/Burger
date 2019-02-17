b58784cb47e527efdbd51c85d30b21060d5f9e04
Pascal Nguyen <pasngu@student.42.fr>
2019-02-17 21:40:38 +0100

    Button controls

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -5,19 +5,26 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 
 const burger = (props) => {
     
-    // const transformedIngredient = Object.keys(props.ingredients)
-    const transformedIngredients = Object.keys(props.ingredients)
+    const transformedIngredient = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
         .map(igkey => {
             return [...Array(props.ingredients[igkey])].map((_, i) => {
                 return <BurgerIngredient key={igkey + i} type={igkey} />
             })
         })
+        .reduce((arr, el) => {
+            return arr.concat(el)
+        }, [])
 
-    // console.log(transformedIngredient)
-    // console.log(transformedIngredients)
-    // console.log(props.ingredients)
-    // console.log(props.ingredients['salad'])
-    // console.log(props.ingredients.salad)
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please start adding ingredients !</p>
+    }
+
+    console.log(transformedIngredient)
+    console.log(transformedIngredients)
+    console.log(props.ingredients)
+    console.log(props.ingredients['salad'])
+    console.log(props.ingredients.salad)
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type='bread-top' />
