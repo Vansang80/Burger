7f7d574f2e05c0e9a1237c8303037de0d5759f7a
Pascal Nguyen <pasngu@student.42.fr>
2019-02-21 03:00:42 +0100

    Add toolbar checkout burger builder

--- src/components/Navigation/ToolBar/ToolBar.js
+++ src/components/Navigation/ToolBar/ToolBar.js
@@ -2,13 +2,14 @@ import React from 'react'
 
 import classes from './ToolBar.css'
 import Logo from '../../Logo/Logo'
+import NavigationItems from '../NavigationItems/NavigationItems'
 
 const toolbar = (props) => (
     <header className={classes.ToolBar}>
         <div>MENU</div>
         <Logo />
         <nav>
-            ...
+            <NavigationItems />
         </nav>
     </header>
 )
