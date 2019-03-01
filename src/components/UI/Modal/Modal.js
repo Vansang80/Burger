73868469ab8b81748f932fb2aa95d17f490e75ec
Pascal Nguyen <pasngu@student.42.fr>
2019-03-02 00:31:13 +0100

    Add a lot componant fix

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,12 +1,19 @@
 import React, {Component} from 'react';
 
 import classes from './Modal.css';
-import Aux from '../../../hoc/Aux';
+import Aux from '../../../hoc/Aux/Aux';
 import Backdrop from '../Backdrop/Backdrop';
 
 class Modal extends Component {
     shouldComponentUpdate (nextProps, nextState) {
-        return nextProps.show !== this.props.show
+        // console.log(nextProps.show, 'nextprops.show')
+        // console.log(this.props.show, 'propsprops.show')
+
+        // console.log(nextProps.children, 'nextpropschildren')
+        // console.log(this.props.children, 'nextpropschildren')
+
+
+        return nextProps.show !== this.props.show || nextProps.children !== this.props.children
     }
 
     render () {
---
+++ src/containers/Checkout/Checkout.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react'
+
+class Checkout extends Componenet {
+
+}
+
+export default Checkout
